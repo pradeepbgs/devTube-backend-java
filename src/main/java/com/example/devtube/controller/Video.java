d936a47a495361d7817893d0da893815a8134d45
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-07 20:11:17 +0530

    made file uploader and its working

--- src/main/java/com/example/devtube/controller/Video.java
+++ src/main/java/com/example/devtube/controller/Video.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.example.devtube.Repository.VideoRepository;
 import com.example.devtube.Repository.userRepository;
 import com.example.devtube.lib.ApiResponse;
+import com.example.devtube.lib.FileUploader;
 import com.example.devtube.models.User;
 import com.example.devtube.service.AuthServie;
 
@@ -31,12 +32,15 @@ private AuthServie authServie;
 @Autowired
 private userRepository userRepository;
 
+@Autowired
+private FileUploader fileUploader;
+
     @PostMapping("/upload")
     public ResponseEntity<ApiResponse> upload (
+        @RequestPart("thumbnail") MultipartFile thumbnail,
         @RequestPart("title") String title,
         @RequestPart("description") String description,
         @RequestPart("video") MultipartFile video,
-        @RequestPart("thumbnail") MultipartFile thumbnail,
         HttpServletRequest request){
         if (title.isEmpty() || title == null) {
             ApiResponse apiResponse = new ApiResponse(400, "pls provide title", null);
@@ -56,8 +60,16 @@ private userRepository userRepository;
         }
         try {
              String loggedInUsername = authServie.getUserFromRequest(request);
-            User user = userRepository.findByUsername(loggedInUsername);
-            return null;
+             User user = userRepository.findByUsername(loggedInUsername);
+            
+             boolean isVideoUploaded = fileUploader.uploadFile(video);
+             boolean isThumbnailSaved = fileUploader.uploadFile(thumbnail);
+            if (!isVideoUploaded || !isThumbnailSaved) {
+                ApiResponse apiResponse = new ApiResponse(400, "didn't saved file`", null);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
+            }
+            ApiResponse apiResponse = new ApiResponse(200,"file saved sucessfully", null);
+            return ResponseEntity.ok(apiResponse);
         } catch (Exception e) {
             ApiResponse apiResponse = new ApiResponse(400,"internal server problem",null);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(apiResponse);
---
+++ src/main/java/com/example/devtube/lib/FileUploader.java
@@ -0,0 +1,42 @@
+package com.example.devtube.lib;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploader {
+    public final String UPLOAD_DIR="/home/pradeep/Desktop/code/devtube/src/main/resources/static/images";
+    public boolean uploadFile(MultipartFile multipartFile){
+        boolean isUploaded = false;
+
+        try {
+            // reading the data
+            // InputStream file = multipartFile.getInputStream();
+            // byte data[] = new byte[file.available()];
+            // file.read(data);
+            // // wil write the data in path
+            // FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+"/"+multipartFile.getOriginalFilename());
+            // fos.write(data);
+            // fos.flush();
+            // fos.close();
+
+            // or we can do like this tooo
+
+            Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+            isUploaded = true; 
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return isUploaded;
+    }
+}
