f68f6471c0feeac025caa37e0147041c7584f4a9
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-22 09:04:57 +0530

    will work more on get video comments

--- src/main/java/com/example/devtube/Repository/CommentRepository.java
+++ src/main/java/com/example/devtube/Repository/CommentRepository.java
@@ -1,13 +1,14 @@
 package com.example.devtube.Repository;
 
-import com.example.devtube.models.CommentModel;
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.example.devtube.models.CommentModel;
+
 public interface CommentRepository extends JpaRepository<CommentModel, Integer> {
-  @Query("SELECT c FROM CommentModel c JOIN FETCH c.user WHERE c.videoId = :videoId")
-  Page<CommentModel> findByVideoId(@Param("videoId") int videoId, Pageable pageable);
+    @Query("SELECT c FROM CommentModel c JOIN FETCH c.user WHERE c.videoId = :videoId")
+    Page<CommentModel> findByVideoId(@Param("videoId") int videoId, Pageable pageable);
 }
--- src/main/java/com/example/devtube/controller/Video.java
+++ src/main/java/com/example/devtube/controller/Video.java
@@ -12,10 +12,7 @@ import com.example.devtube.service.AuthService;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.hibernate.query.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +22,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 @RestController
 @RequestMapping("/api/video")
